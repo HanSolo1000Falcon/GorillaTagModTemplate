cbaa45be68f931212cdb01a8ed328ddc8fd5d64b
HanSolo1000Falcon <HanSolo1000Falcon.business@gmail.com>
2025-08-05 02:17:51 +0200

    Add files via upload

---
+++ Constants.cs
@@ -0,0 +1,9 @@
+namespace GorillaTagModTemplate
+{
+    public class Constants
+    {
+        public const string PluginGuid = "REPLACEMEAUTHORNAMETOLOWER.REPLACEMEMODNAMETOLOWER";
+        public const string PluginName = "GorillaTagModTemplate";
+        public const string PluginVersion = "1.0.0";
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,20 @@
+﻿using BepInEx;
+using GorillaTagModTemplate.Patches;
+
+namespace GorillaTagModTemplate
+{
+    [BepInPlugin(Constants.PluginGuid, Constants.PluginName, Constants.PluginVersion)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Start()
+        {
+            HarmonyPatches.ApplyHarmonyPatches();
+            GorillaTagger.OnPlayerSpawned(OnGameInitialized);
+        }
+
+        private void OnGameInitialized()
+        {
+            
+        }
+    }
+}
